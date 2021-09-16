7d21adebefb494427de113b57db8bc7ae502069a
jeong-hyuk <jack1150@naver.com>
2021-09-16 21:15:36 +0900

    using first intelliJ

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -1,6 +1,6 @@
 public class JavaPractice {
     public static void main(String[] args){
         System.out.println("안녕하세요");
-        System.out.println("안녕하세요");
+        System.out.println("첫 인텔리제이 사용");
     }
 }
